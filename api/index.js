36740fc758c3244cb24fc6cf3035df79215cce1d
robloxguys777-cmd <robloxguys777@gmail.com>
2026-07-08 20:51:29 -0500

    Add webhook route for Sellauth

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const licenseRoutes = require('./routes/license');
+const handleWebhook = require('./routes/webhooks');
 
 const app = express();
 app.use(express.json());
 
 app.use('/license', licenseRoutes);
+app.post('/webhooks/sellauth', handleWebhook);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
