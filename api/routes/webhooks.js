ceaaca5d92a6557d8e6f5c53d7edccaa4202058b
robloxguys777-cmd <robloxguys777@gmail.com>
2026-07-08 21:15:15 -0500

    Remove duplicate imports in webhooks.js

--- api/routes/webhooks.js
+++ api/routes/webhooks.js
@@ -1,14 +1,9 @@
 const crypto = require('crypto');
 const https = require('https');
 
-const crypto = require('crypto');
-const https = require('https');
-
 const SELLAUTH_WEBHOOK_SECRET = process.env.SELLAUTH_WEBHOOK_SECRET || '';
 const SELLAUTH_API_KEY = process.env.SELLAUTH_API_KEY || '';
 const SELLAUTH_SHOP_ID = Number(process.env.SELLAUTH_SHOP_ID || 0);
-
-const BASE = 'https://api.sellauth.com/api';
 
 const BASE = 'https://api.sellauth.com/api';
 
